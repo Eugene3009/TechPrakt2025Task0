6f2244a4d8c30b5ad436c1410a8ad82aff60d9c4
Eugene <ipz233_byev@student.ztu.edu.ua>
2025-01-23 15:56:12 +0200

    Create calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть два числа");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Сума: {x + y}");
+        }
+    }
+}
